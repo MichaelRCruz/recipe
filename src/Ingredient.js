9fc3aa7b0765f2b0a17065f2b25a94323784df69
Michael Ramon Cruz <>
2021-01-18 19:24:45 -0600

    add specials description

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import SessionContext from './SessionContext.js';
 
 const Ingredient = props => {
 
+  const sessionContext = useContext(SessionContext);
   const { uuid, name, specialIngredientsIds } = props;
-
+  const { specialIngredients } = sessionContext.state;
   
   return specialIngredientsIds.includes(uuid)
       ? <div>
-          <p>{name} and {uuid}</p>
+          <p>{name} also on sale</p>
+          <p>{specialIngredients[uuid].title}</p>
+          <p>{specialIngredients[uuid].type}</p>
+          <p>{specialIngredients[uuid].text}</p>
         </div>
       : (
         <div>
